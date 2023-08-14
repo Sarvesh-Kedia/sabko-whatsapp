4ba38b1f7a5e17b2db43289fca66f1f12dce5a95
Sarvesh-Kedia <sarvesh512kedia@gmail.com>
2023-08-14 23:39:31 +0530

    made india code default

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ function sendToWhatsAppChat() {
     let phoneNumber = document.getElementById('phoneNumber').value;
     let message = document.getElementById('textMessage').value;
 
-    let url =  'https://wa.me/' + phoneNumber + '?text=' + message
+    let url =  'https://wa.me/91' + phoneNumber + '?text=' + message
 
     window.open(url,'_blank');
 
